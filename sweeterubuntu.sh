f7de68c5ef648e8e5aac474e4ef7b23db02ff80d
Mikael <mikael@rosel.se>
2019-10-16 13:07:47 -0700

    up

--- sweeterubuntu.sh
+++ sweeterubuntu.sh
@@ -11,36 +11,36 @@ gsettings set org.gnome.shell.keybindings switch-to-application-7 []
 gsettings set org.gnome.shell.keybindings switch-to-application-8 []
 gsettings set org.gnome.shell.keybindings switch-to-application-9 []
 
-Enable super+nr to switch workspace
+# Enable super+nr to switch workspace
 
 
-Disable Workspace switcher popup
-sudo nano /usr/share/gnome-shell/theme/ubuntu.css
-########### BYT #################
-/* Workspace Switcher */
-.workspace-switcher-group {
-  padding: 2px; }
+# Disable Workspace switcher popup
+# sudo nano /usr/share/gnome-shell/theme/ubuntu.css
+# ########### BYT #################
+# /* Workspace Switcher */
+# .workspace-switcher-group {
+#   padding: 2px; }
 
-.workspace-switcher {
-  background: transparent;
-  border: 0px;
-  border-radius: 0px;
-  padding: 0px;
-  spacing: 6px; }
+# .workspace-switcher {
+#   background: transparent;
+#   border: 0px;
+#   border-radius: 0px;
+#   padding: 0px;
+#   spacing: 6px; }
 
-.ws-switcher-active-up, .ws-switcher-active-down {
-  height: 5px;
-  background-color: #dd4814;
-  color: #ffffff;
-  background-size: 2px;
-  border-radius: 2px; }
+# .ws-switcher-active-up, .ws-switcher-active-down {
+#   height: 5px;
+#   background-color: #dd4814;
+#   color: #ffffff;
+#   background-size: 2px;
+#   border-radius: 2px; }
 
-.ws-switcher-box {
-  height: 5px;
-  border: 1px solid rgba(238, 238, 236, 0.1);
-  background: transparent;
-  border-radius: 2px; }
-#####################################
+# .ws-switcher-box {
+#   height: 5px;
+#   border: 1px solid rgba(238, 238, 236, 0.1);
+#   background: transparent;
+#   border-radius: 2px; }
+# #####################################
 
-Remove Snaps
-sudo apt autoremove --purge snapd gnome-software-plugin-snap 
+# Remove Snaps
+# sudo apt autoremove --purge snapd gnome-software-plugin-snap 
