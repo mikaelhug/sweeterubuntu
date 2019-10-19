912bfc00dfda7c91415ab1b935c8334cdc231c0b
Mikael <mikael@rosel.se>
2019-10-19 22:50:12 +0200

    Ask

--- sweeterubuntu.sh
+++ sweeterubuntu.sh
@@ -1,6 +1,8 @@
 
 
 ####### UPDATE SYSTEM #######
+echo "Updating System!\n\n"
+sleep 2
 sudo apt update
 sudo apt upgrade -y
 sudo apt install curl wget -y
@@ -9,41 +11,49 @@ sudo apt install curl wget -y
 
 ####### PACKAGE CONFIG #######
 # Remove Snaps | because snaps are evil
-sudo rm -rf /var/cache/snapd/
-sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
-rm -rf ~/snap
+read -r -p "Remove Snaps becuase they are evil? [y/N] " response
+if [ "$response" = "y" ]
+then
+   sudo rm -rf /var/cache/snapd/
+   sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
+   rm -rf ~/snap
+fi
 # -->
 
 # Nextcloud PPA
-test -f /etc/apt/sources.list.d/nextcloud-devs-ubuntu-client-bionic.list
-if [ "$?" != "0" ];
+read -r -p "Add Nextcloud ppa and install? [y/N] " response
+if [ "$response" = "y" ]
 then
    sudo add-apt-repository ppa:nextcloud-devs/client -y
    sudo apt update
+   sudo apt install nextcloud-client -y
 fi
-sudo apt install nextcloud-client -y
 # -->
 
 # Spotify
-test -f /etc/apt/sources.list.d/spotify.list
-if [ "$?" != "0" ];
+read -r -p "Add Spotify repo and install? [y/N] " response
+if [ "$response" = "y" ]
 then
    curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
    sudo apt update
+   sudo apt install spotify-client -y
 fi
-sudo apt install spotify-client -y
 # -->
 
 # Install packages
-sudo apt update
-sudo apt install chromium-browser thunderbird libreoffice gnome-maps remmina gnome-system-monitor -y
-sudo apt install python3-pip git gnome-calculator -y
+read -r -p "Install great packages? [y/N] " response
+if [ "$response" = "y" ]
+then
+   sudo apt update
+   sudo apt install chromium-browser thunderbird libreoffice gnome-maps remmina gnome-system-monitor -y
+   sudo apt install python3-pip git gnome-calculator pdfsam -y
+fi
 # -->
 
 # Slack
-pkg-query --show "slack-desktop"
-if [ "$?" != "0" ];
+read -r -p "Install Slack? [y/N] " response
+if [ "$response" = "y" ]
 then
    sudo apt install gconf2 python libappindicator1 libindicator7 -y
    cd ~/Downloads
@@ -54,39 +64,48 @@ fi
 # -->
 
 # VScode
-test -f /etc/apt/sources.list.d/vscode.list
-if [ "$?" != "0" ];
+read -r -p "Install Visual Studio Code? [y/N] " response
+if [ "$response" = "y" ]
 then
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo apt update
-
-    cd ~/Downloads
-    wget https://raw.githubusercontent.com/mhugen/sweeterconfigs/master/vscode/settings.json
-    wget https://raw.githubusercontent.com/mhugen/sweeterconfigs/master/vscode/keybindings-linux.json
-    rm -f ~/.config/Code/User/settings.json
-    rm -f ~/.config/Code/User/keybindings.json
-    mv settings.json ~/.config/Code/User/
-    mv keybindings-linux.json ~/.config/Code/User/
+   curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+   sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+   sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+   sudo apt update
+   sudo apt install code -y
 fi
-sudo apt install code -y
-code --install-extension ms-python.python
-code --install-extension vscodevim.vim
-code --install-extension rafamel.subtle-brackets
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension cssho.vscode-svgviewer
+
+read -r -p "Install VScode settings and extensions? [y/N] " response
+if [ "$response" = "y" ]
+then
+   cd ~/Downloads
+   wget https://raw.githubusercontent.com/mhugen/sweeterconfigs/master/vscode/settings.json
+   wget https://raw.githubusercontent.com/mhugen/sweeterconfigs/master/vscode/keybindings-linux.json
+   rm -f ~/.config/Code/User/settings.json
+   rm -f ~/.config/Code/User/keybindings.json
+   mv settings.json ~/.config/Code/User/
+   mv keybindings-linux.json ~/.config/Code/User/
+
+   code --install-extension ms-python.python
+   code --install-extension vscodevim.vim
+   code --install-extension rafamel.subtle-brackets
+   code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+   code --install-extension ms-vscode.cpptools
+   code --install-extension ms-azuretools.vscode-docker
+   code --install-extension cssho.vscode-svgviewer
+fi
 # -->
 
 # Jupyter Lab
-/usr/bin/python3 -m pip install -U matplotlib numpy scipy jupyterlab pylint --user
+read -r -p "Install matplotlib numpy scipy jupyterlab pylint? [y/N] " response
+if [ "$response" = "y" ]
+then
+   /usr/bin/python3 -m pip install -U matplotlib numpy scipy jupyterlab pylint --user
+fi
 # -->
 
 # Firefox uBlock Origin, https everywhere, privacy badger,floccus, vimium
-test -f ~/.mozilla/firefox/*.default-release/extensions/uBlock0@raymondhill.net.xpi
-if [ "$?" != "0" ];
+read -r -p "Install Firefox plugins? [y/N] " response
+if [ "$response" = "y" ]
 then
    cd ~/.mozilla/firefox/*.default-release/extensions
    wget https://addons.mozilla.org/firefox/downloads/latest/607454/addon-607454-latest.xpi -O uBlock0@raymondhill.net.xpi
@@ -98,8 +117,8 @@ fi
 # -->
 
 # Download Chromium web-apps | create with terminal?
-test -f ~/Apps/chrome-ncdkolgolkbieejnpdjkkihfbdmcdpnj-Default.desktop
-if [ "$?" != "0" ];
+read -r -p "Install Chromium apps? [y/N] " response
+if [ "$response" = "y" ]
 then
    mkdir -p ~/Apps
    cd ~/Apps
@@ -115,8 +134,8 @@ fi
 # -->
 
 # Thunderbird lightning
-test -f ~/.thunderbird/*.default/extensions/{e2fda1a4-762b-4020-b5ad-a41df1933103}.xpi
-if [ "$?" != "0" ];
+read -r -p "Add Lightning to Thunderbird? [y/N] " response
+if [ "$response" = "y" ]
 then
    mkdir -p ~/.thunderbird/*.default/extensions
    cd ~/.thunderbird/*.default/extensions
@@ -128,82 +147,87 @@ fi
 
 
 ####### UI CONFIG #######
-# Disable super+{1,2,3,..} to launch apps
-gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-1 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-2 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-3 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-4 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-5 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-6 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-7 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-8 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-9 []
-gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-10 []
-# -->
 
-# Enable 10 workspaces by default
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
-# -->
+read -r -p "Use custom Ubuntu features? [y/N] " response
+if [ "$response" = "y" ]
+then
+   # Disable super+{1,2,3,..} to launch apps
+   gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-1 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-2 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-3 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-4 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-5 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-6 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-7 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-8 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-9 []
+   gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-10 []
+   # -->
 
-# Enable super+nr to switch workspace | add move to workspace shortcut?
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
-# -->
+   # Enable 10 workspaces by default
+   gsettings set org.gnome.mutter dynamic-workspaces false
+   gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+   # -->
 
-# Tab only active workspace and isolate that workspace
-gsettings set org.gnome.shell.app-switcher current-workspace-only true
-gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
-# -->
+   # Enable super+nr to switch workspace | add move to workspace shortcut?
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+   # -->
 
-# Change alt+tab to switch window and suber+tab to switch application
-gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
-# -->
+   # Tab only active workspace and isolate that workspace
+   gsettings set org.gnome.shell.app-switcher current-workspace-only true
+   gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true
+   # -->
 
-# Some useful keyboard shortcuts
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal '<Super>t'
-gsettings set org.gnome.settings-daemon.plugins.media-keys home '<Super>f'
-gsettings set org.gnome.settings-daemon.plugins.media-keys www '<Super>w'
-gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>r']"
-# -->
+   # Change alt+tab to switch window and suber+tab to switch application
+   gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+   gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+   # -->
 
-# Dock visuals: dock size 28, 
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES'
-# -->
+   # Some useful keyboard shortcuts
+   gsettings set org.gnome.settings-daemon.plugins.media-keys terminal '<Super>t'
+   gsettings set org.gnome.settings-daemon.plugins.media-keys home '<Super>f'
+   gsettings set org.gnome.settings-daemon.plugins.media-keys www '<Super>w'
+   gsettings set org.gnome.desktop.wm.keybindings panel-run-dialog "['<Super>r']"
+   # -->
 
-# Move titlebar buttons to the left
-gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
-# -->
+   # Dock visuals: dock size 28, 
+   gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 28
+   gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES'
+   # -->
 
-# Use expanded list view
-gsettings set org.gnome.nautilus.list-view use-tree-view true
-# -->
+   # Move titlebar buttons to the left
+   gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+   # -->
 
-# Show battery percentage
-gsettings set org.gnome.desktop.interface show-battery-percentage true
-# -->
+   # Use expanded list view
+   gsettings set org.gnome.nautilus.list-view use-tree-view true
+   # -->
 
-# Some date options
-gsettings set org.gnome.desktop.interface clock-show-seconds true
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.calendar show-weekdate true
-# -->
+   # Show battery percentage
+   gsettings set org.gnome.desktop.interface show-battery-percentage true
+   # -->
 
-# Disable animations
-gsettings set org.gnome.desktop.interface enable-animations false
+   # Some date options
+   gsettings set org.gnome.desktop.interface clock-show-seconds true
+   gsettings set org.gnome.desktop.interface clock-show-date true
+   gsettings set org.gnome.desktop.calendar show-weekdate true
+   # -->
+
+   # Disable animations
+   gsettings set org.gnome.desktop.interface enable-animations false
+fi
 # -->
 
 # Install some extensions | poor with apt. Zip?
@@ -214,38 +238,26 @@ gsettings set org.gnome.desktop.interface enable-animations false
 # gsettings set org.gnome.shell enabled-extensions "['workspace-indicator@gnome-shell-extensions.gcampax.github.com']"
 
 
-# Disable Workspace switcher popup
-# sudo nano /usr/share/gnome-shell/theme/ubuntu.css
-# ########### BYT #################
-# /* Workspace Switcher */
-# .workspace-switcher-group {
-#   padding: 2px; }
+# Make Workspace switcher popup smaller
+read -r -p "Make Workspace switcher and panel spacing smaller? [y/N] " response
+if [ "$response" = "y" ]
+then
+   config=/usr/share/gnome-shell/theme/ubuntu.css
+   sudo sed -i '/^.workspace-switcher-group/,/}$/ s/padding:.*/padding: 2px; }/g' $config
+   sudo sed -i '/^.workspace-switcher/,/}$/ s/spacing:.*/spacing: 6px; }/g' $config
+   sudo sed -i '/^.ws-switcher-active-up, .ws-switcher-active-down/,/}$/ s/height:.*/height: 5px;/g' $config
+   sudo sed -i '/^.ws-switcher-active-up, .ws-switcher-active-down/,/}$/ s/background-size:.*/background-size: 2px;/g' $config
+   sudo sed -i '/^.ws-switcher-active-up, .ws-switcher-active-down/,/}$/ s/border-radius:.*/border-radius: 2px; }/g' $config
+   sudo sed -i '/^.ws-switcher-box/,/}$/ s/height:.*/height: 5px;/g' $config
+   sudo sed -i '/^.ws-switcher-box/,/}$/ s/border-radius:.*/border-radius: 2px; }/g' $config
 
-# .workspace-switcher {
-#   background: transparent; Bookmark 
-#   border: 0px;
-#   border-radius: 0px;
-#   padding: 0px;
-#   spacing: 6px; }
-
-# .ws-switcher-active-up, .ws-switcher-active-down {
-#   height: 5px;
-#   background-color: #dd4814;
-#   color: #ffffff;
-#   background-size: 2px;
-#   border-radius: 2px; }
-
-# .ws-switcher-box {
-#   height: 5px;
-#   border: 1px solid rgba(238, 238, 236, 0.1);
-#   background: transparent;
-#   border-radius: 2px; }
-# #####################################
-
-# /usr/share/gnome-shell/theme/gnome-shell.css
-#panel .panel-button {
-# -natural-hpadding: 4px;
-# -minimum-hpadding: 4px;
-
-
+   # Remove wide spacing panel icons
+   config=/usr/share/gnome-shell/theme/gnome-shell.css
+   sudo sed -i '/^  #panel .panel-button/,/}$/ s/-natural-hpadding:.*/-natural-hpadding: 4px;/g' $config
+   sudo sed -i '/^  #panel .panel-button/,/}$/ s/-minimum-hpadding:.*/-minimum-hpadding: 4px;/g' $config
+fi
+# -->
 ####### END UI CONFIG #######
+
+
+echo "\n\nYou should restart your computer now.\n\n"
